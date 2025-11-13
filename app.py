b51ef19ba0885ddb32e338a974a19ca93d0418f3
livot7 <livot7@outlook.com>
2025-11-13 14:20:49 -0300

    Base head component and blueprint

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from flask_migrate import Migrate
 
 from config import Config
 from modelos.modelo import db
-from controladores.controlador_usuario import usuario_blueprint
+from controladores.controlador_principal import principal_blueprint
+from controladores.controlador_painel import painel_blueprint
+from controladores.controlador_admin import admin_blueprint
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,7 +15,9 @@ db.init_app(app)
 migrate = Migrate(app, db)
 
 # Registra as blueprints
-app.register_blueprint(usuario_blueprint)
+app.register_blueprint(principal_blueprint)
+app.register_blueprint(painel_blueprint)
+app.register_blueprint(admin_blueprint)
 
 if __name__ == "__main__":
     app.run(debug=True)
