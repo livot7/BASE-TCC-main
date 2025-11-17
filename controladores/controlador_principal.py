39d7d299815a093e7701e988d66d560f45a60ba6
rafaelleeto <leetobr@gmail.com>
2025-11-17 14:33:10 -0300

    Autenticação de login

--- controladores/controlador_principal.py
+++ controladores/controlador_principal.py
@@ -1,7 +1,26 @@
-from flask import Blueprint, request, render_template, redirect
+from flask import Blueprint, request, render_template, redirect, session
+from modelos import Moderador
+from werkzeug.security import check_password_hash
+
 
 principal_blueprint = Blueprint("principal", __name__, template_folder="../vistas/templates")
 
-@principal_blueprint.route("/")
+@principal_blueprint.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("login.html")
+    if request.method == "GET":
+        return render_template("login.html")
+    
+    senha = request.form["senha"]
+    email = request.form["email"]
+
+    moderador = Moderador.query.filter_by(email=email).first()
+    if moderador is None:
+        return redirect("/")
+    
+    if not check_password_hash(moderador.senha_hash, senha):
+        return redirect("/")
+    
+    session["usuario"] = moderador.id
+    return redirect("/painel")
+    
+    
