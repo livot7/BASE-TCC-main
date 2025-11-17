06b8c71f1579a2ae3f6ebdb0e3a39f8993c6dc21
rafaelleeto <leetobr@gmail.com>
2025-11-17 14:47:46 -0300

    Autenticação para o painel e logout

--- controladores/controlador_painel.py
+++ controladores/controlador_painel.py
@@ -1,7 +1,17 @@
-from flask import Blueprint, request, render_template, redirect
+from flask import Blueprint, request, render_template, redirect, session
 
 painel_blueprint = Blueprint("painel", __name__, template_folder="../vistas/templates")
+
+@painel_blueprint.before_request
+def painel_before_request():
+    if session.get("usuario") is None:
+        return redirect("/")
 
 @painel_blueprint.route("/painel")
 def painel_home():
     return render_template("home.html")
+
+@painel_blueprint.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
