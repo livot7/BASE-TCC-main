367367337e84eb89c624bd8a132b07f34dfbb1bf
rafaelleeto <leetobr@gmail.com>
2025-11-17 16:41:56 -0300

    Criar moderador

--- cli.py
+++ cli.py
@@ -6,6 +6,7 @@ from modelos.moderador import Moderador
 
 parser = argparse.ArgumentParser(description="VIA")
 parser.add_argument("--criar_admin", help="Cria um admin", action="store_true")
+parser.add_argument("--criar_moderador", help="Cria um admin", action="store_true")
 
 args = parser.parse_args()
 
@@ -22,5 +23,21 @@ if args.criar_admin:
             # Criptografia da senha
             senha_hash=generate_password_hash(senha),
             admin=True,
+        )
+        novo_admin.salvar()
+
+if args.criar_moderador:
+    nome = input("Nome: ")
+    email = input("Email: ")
+    senha = input("Senha: ")
+
+    # Como estamos fazendo isso por fora do flash, temos que chamar essa função
+    with app.app_context():
+        novo_admin = Moderador(
+            nome=nome,
+            email=email,
+            # Criptografia da senha
+            senha_hash=generate_password_hash(senha),
+            admin=False,
         )
         novo_admin.salvar()
