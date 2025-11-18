e30426f3ab7121f89bb39f9952b6c4fc215c551e
rafaelleeto <leetobr@gmail.com>
2025-11-18 15:41:12 -0300

    Esconde botões de administradores para moderadores

--- controladores/controlador_painel.py
+++ controladores/controlador_painel.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request, render_template, redirect, session
+from modelos import Moderador
 
 painel_blueprint = Blueprint(
     "painel", __name__, template_folder="../vistas/templates")
@@ -12,7 +13,8 @@ def painel_before_request():
 
 @painel_blueprint.route("/painel")
 def painel_home():
-    return render_template("home.html")
+    moderador = Moderador.query.get_or_404(session["usuario"])
+    return render_template("home.html", moderador=moderador)
 
 
 @painel_blueprint.route("/logout")
