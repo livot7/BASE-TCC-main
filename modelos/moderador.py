4e35b2696a8be4df734a0a86711057eb4a350517
livot7 <livot7@outlook.com>
2025-11-26 16:55:47 -0300

    Adicionar coluna ativo nos moderadores

--- modelos/moderador.py
+++ modelos/moderador.py
@@ -1,3 +1,4 @@
+import sqlalchemy
 from modelos.modelo import db, Modelo
 
 
@@ -8,6 +9,8 @@ class Moderador(Modelo):
     email = db.Column(db.String(120), unique=True, nullable=False)
     senha_hash = db.Column(db.String(512), nullable=False)
     admin = db.Column(db.Boolean, nullable=False)
+    ativo = db.Column(db.Boolean, nullable=False, default=True,
+                      server_default=sqlalchemy.true())
 
     def verificar_usuario(self):
         pass
