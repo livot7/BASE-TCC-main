0ca864d2bf39a6f9473b1c9b366e13269e1a7fed
livot7 <livot7@outlook.com>
2025-11-26 15:14:47 -0300

    Esconde chave secretas em arquivo .env

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://neondb_owner:npg_vqIRaegOyB09@ep-weathered-dew-acld0qqw-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require"
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # TODO: Esconder chave secreta
-    SECRET_KEY = "dev"
+    SECRET_KEY = os.getenv("SECRET_KEY")
